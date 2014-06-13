a41cbd3c4762064f4600a6b7ffe1587be96a6691
helenswanson <hhyswanson@gmail.com>
2014-06-13 10:01:50 -0400

    Initial commit. Created job index view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Muenster::Application.routes.draw do
+  resources :jobs, only: :index
+
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
